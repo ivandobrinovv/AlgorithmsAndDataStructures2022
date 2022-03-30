7b0037a70f6d88188bb868ecf681c3ef2f4b1425
Ivan Peev <idobrinovv@gmail.comivan>
2022-03-30 13:53:42 +0300

    Add a solution that finds a path between two vertices and its weight

---
+++ Graphs/Graphs/Helpers/GraphHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Graphs.Helpers
+{
+    public static class GraphHelper
+    {
+        public static int PathWeight(int[,] nodes, int vertex1, int vertex2, List<int> checkedVertices, int currentWeight)
+        {
+            if(IsThereADirectPath(nodes, vertex1, vertex2))
+            {
+                return currentWeight + nodes[vertex1, vertex2];
+            }
+
+            checkedVertices.Add(vertex1);
+
+            for (int i = 0; i < nodes.GetLength(1); i++)
+            {
+                if(IsThereADirectPath(nodes, vertex1, i)
+                    && !checkedVertices.Contains(i))
+                {
+                    return PathWeight(nodes, i, vertex2, checkedVertices, currentWeight + nodes[vertex1, i]);
+                }
+            }
+
+            return 0;
+        }
+
+        public static bool IsThereADirectPath(int[,] nodes, int vertex1, int vertex2)
+        {
+            if (nodes[vertex1, vertex2] != 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Graphs/Graphs/Program.cs
@@ -0,0 +1,15 @@
+﻿using Graphs.Helpers;
+
+int[,] nodes = new int[,]{
+    { 0, 8, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 4, 0 },
+    { 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0 }
+    };
+
+List<int> checkedVertices = new List<int>();
+
+Console.WriteLine(GraphHelper.PathWeight(nodes, 0, 4, checkedVertices, 0));
+
